a70084b592223e2b7a70160677ef3bae3615f9c7
Samantha Wang <yuhanwang-inaus@hotmail.com>
2025-07-10 16:15:26 +1000

    implements creation of topics

--- index.js
+++ index.js
@@ -6,3 +6,28 @@ const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+// session memory
+var topics = [];
+
+// homepage
+app.get("/", (req, res) => {
+  res.render("index.ejs", {curTopic: topics});
+});
+
+// create new topic
+app.get("/newTopic", (req, res) => {
+  res.render("newTopic.ejs");
+});
+app.post("/newTopic", (req, res) => {
+  var topic = {
+    title: req.body["topic title"],
+    content: []
+  };
+  topics.push(topic);
+  res.redirect("/");
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
